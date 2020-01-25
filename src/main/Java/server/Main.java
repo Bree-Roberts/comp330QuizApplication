5f0d5c2765ab91f7ed7c73162ea60205153e2a6b
ErikCG <erikcgreve@gmail.com>
2020-01-24 18:00:52 -0600

    added comments

--- src/main/Java/server/Main.java
+++ src/main/Java/server/Main.java
@@ -11,17 +11,19 @@ public class Main {
 
   public static void main(String[] args) throws Exception {
     Object obj = new JSONParser().parse(new FileReader("src/main/java/server/questions.json"));
-    PrintStream out = new PrintStream(System.out);
-
+    PrintStream out = new PrintStream(System.out); //Allows to pass an output to other methods. We can change where the output goes later. (System.out for now)
+    Scanner in = new Scanner(System.in);
     Parser parser = new Parser(obj);
     Output output = new Output(out);
 
+    //Parse the questions and answers
     Map<String, String> trueFalse = parser.parseTrueFalse();
     Map<String, String> matching = parser.parseMatching();
-    Scanner in = new Scanner(System.in);
 
+    //Ask the questions and record score
     int trueFalseScore = output.askTrueFalse(trueFalse, in);
     int matchingScore = output.askMatching(matching, in);
+
     out.println(
         "You scored "
             + trueFalseScore
