2c8d2bbb9624dc093f463817a2421906b9bac578
Bturner314 <bturner3@luc.edu>
2020-01-27 11:41:19 -0600

    Added code to show the ui

--- src/main/Java/server/Main.java
+++ src/main/Java/server/Main.java
@@ -28,32 +28,33 @@ public class Main extends Application{
 	private Scanner in = new Scanner(System.in);
 	private Parser parser = new Parser(obj);
 	private Output output = new Output(out);
-	
+
 	//UI Elements
 	private BorderPane mainLayout = new BorderPane();
 	private HBox centerWindow = new HBox();
 	private Scene mainScene = new Scene(mainLayout, 900, 600);
 	private Text questionOutput = new Text(); 
 	private ScrollPane questionOutputWindow = new ScrollPane();
-	
+
 	private Label scoreBoard = new Label();
-	
+
 	public void start(Stage primaryStage) throws Exception {
-		
+
 		primaryStage.setTitle("Quiz Game");
 		mainLayout.setCenter(centerWindow);
 		mainLayout.setTop(scoreBoard);
 		questionOutputWindow.setContent(questionOutput);
-		
+		primaryStage.setScene(mainScene);
+
 		//UI formatting
 		questionOutput.setFont(new Font(20));
 		questionOutput.setWrappingWidth(620);
-		
-		
+
+
 		//binds Label text
 		scoreBoard.textProperty().bind(ScoreBoard.getScoreBoard());
 		questionOutput.textProperty().bind(QuestionOutput.getQuestionOutputText());
-		
+
 		//Parse the questions and answers
 		Map<String, String> trueFalse = parser.parseTrueFalse();
 		Map<String, String> matching = parser.parseMatching();
@@ -75,7 +76,7 @@ public class Main extends Application{
 						+ matching.size()
 						+ " on the Matching section.");
 
-
+		primaryStage.show();
 
 	}
 }
